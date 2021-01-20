f7551d60ce3d14bef97208e288a95ae45c351a5a
iamzhaoqing <zhaoq@0104@163.com>
2021-01-20 13:26:26 +0800

    fix: 修改构建逻辑

--- publish.sh
+++ publish.sh
@@ -1,15 +1,19 @@
 #! /usr/bin/env bash
 
+set -e
 node build.js
-
-cp -r package.json ./dist/package.json
-cp -r package.json ./dist/v2/package.json
 
 cp -r index.js ./dist/index.js
 cp -r index.js ./dist/v2/index.js
 
 cp -r generator ./dist/generator
 cp -r generator ./dist/v2/generator
+
+cp -r prompts.js ./dist/prompts.js
+cp -r prompts.js ./dist/v2/prompts.js
+
+cp -r README.md ./dist/README.md
+cp -r LICENSE ./dist/LICENSE
 
 cd dist
 npm publish
