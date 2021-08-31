f146d3568c595ba79a979e16961da4bd88a1440a
izhaoqing <zhaoq@0104@163.com>
2021-08-31 10:04:36 +0800

    perf(axios): 优化响应拦截器逻辑

--- generator/template-base/src/plugins/axios.js
+++ generator/template-base/src/plugins/axios.js
@@ -36,49 +36,23 @@ _axios.interceptors.response.use(
         return response;
     },
     function(error) {
+        error.message = '连接服务器失败';
         if (error.response) {
-            switch (error.response.status) {
-                case 400:
-                    error.message = '错误请求';
-                    break;
-                case 401:
-                    error.message = '未授权，请重新登录';
-                    break;
-                case 403:
-                    error.message = '拒绝访问';
-                    break;
-                case 404:
-                    error.message = '请求错误，未找到该资源';
-                    break;
-                case 405:
-                    error.message = '请求方法未允许';
-                    break;
-                case 408:
-                    error.message = '请求超时';
-                    break;
-                case 500:
-                    error.message = '服务端出错';
-                    break;
-                case 501:
-                    error.message = '网络未实现';
-                    break;
-                case 502:
-                    error.message = '网络错误';
-                    break;
-                case 503:
-                    error.message = '服务不可用';
-                    break;
-                case 504:
-                    error.message = '网络超时';
-                    break;
-                case 505:
-                    error.message = 'http版本不支持该请求';
-                    break;
-                default:
-                    error.message = `连接错误${error.response.status}`;
+            const statusMap = {
+                400: '错误请求',
+                401: '未授权，请重新登录',
+                403: '拒绝访问',
+                404: '请求错误，未找到该资源',
+                405: '请求方法未允许',
+                408: '请求超时',
+                500: '服务端出错',
+                501: '网络未实现',
+                502: '网络错误',
+                503: '服务不可用',
+                504: '网络超时',
+                505: 'http版本不支持该请求',
             }
-        } else {
-            error.message = '连接服务器失败';
+            error.message = statusMap[error.response.status] || `连接错误${error.response.status}`;
         }
         return Promise.reject(error);
     },
